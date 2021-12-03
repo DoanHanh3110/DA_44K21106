8d416a685e34a1c7e025cba36f0ecffc4af7b5f2
unknown <dotrungdat154@gmail.com>
2021-12-04 02:20:50 +0700

    update
    chỉnh sửa json

--- java/login.js
+++ java/login.js
@@ -4,7 +4,7 @@ function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "admin" && password == "admin"){
-alert ("Login successfully");
+alert ("Đăng nhập thành công!");
 window.location = "index.html"; // Redirecting to other page.
 return false;
 }
